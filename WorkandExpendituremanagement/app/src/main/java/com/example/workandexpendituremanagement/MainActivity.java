0b5e8099db4ad7d482d24210ed2732eb02875c20
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2021-06-10 15:46:23 +0700

    upload main view
    code thiết kế giao diện cho trang chủ:  phần bottom navigation

--- WorkandExpendituremanagement/app/src/main/java/com/example/workandexpendituremanagement/MainActivity.java
+++ WorkandExpendituremanagement/app/src/main/java/com/example/workandexpendituremanagement/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setSelectedItemId(R.id.page_1);
+
     }
 }
